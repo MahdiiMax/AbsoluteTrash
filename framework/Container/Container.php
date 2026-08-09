4989b7d96eb15eafe1523eb6a8fad8efab2d6d45
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-10 00:13:11 +0330

    feat: add reflection autowiring

--- framework/Container/Container.php
+++ framework/Container/Container.php
@@ -6,6 +6,8 @@ namespace Trash\Container;
 
 use Closure;
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
+use ReflectionNamedType;
 use Trash\Container\Exceptions\NotFoundException;
 
 class Container implements ContainerInterface
@@ -14,15 +16,53 @@ class Container implements ContainerInterface
     private array $singletonBindings = [];
     private array $instances = [];
 
+    private function build(string $class, array $parameters = []): object
+    {
+        $reflection = new ReflectionClass($class);
+        if (!$reflection->isInstantiable()) {
+            throw new NotFoundException("Target [$class] is not instantiable.");
+        }
+        $constructor = $reflection->getConstructor();
+        if ($constructor === null) {
+            return $reflection->newInstance();
+        }
+        $arguments = [];
+        foreach ($constructor->getParameters() as $parameter) {
+            $name = $parameter->getName();
+            if (array_key_exists($name, $parameters)) {
+                $arguments[] = $parameters[$name];
+                continue;
+            }
+            $type = $parameter->getType();
+            if ($type instanceof ReflectionNamedType && !$type->isBuiltin()) {
+                $service = $type->getName();
+                if ($this->has($service)) {
+                    $arguments[] = $this->get($service);
+                    continue;
+                }
+            }
+            if ($parameter->isDefaultValueAvailable()) {
+                $arguments[] = $parameter->getDefaultValue();
+                continue;
+            }
+            if ($parameter->isVariadic()) {
+                continue;
+            }
+
+            throw new NotFoundException("Unable to resolve parameter [\${$name}] for class [{$class}].");
+        }
+        return $reflection->newInstanceArgs($arguments);
+    }
+
     private function resolve(callable|string $concrete, array $parameters = []): mixed
     {
         if ($concrete instanceof Closure) {
             return $concrete($this);
         }
-        return new $concrete();
+        return $this->build($concrete, $parameters);
     }
 
-    public function bind(string  $abstract, callable|string|null $concrete = null): void
+    public function bind(string $abstract, callable|string|null $concrete = null): void
     {
         $this->bindings[$abstract] = $concrete ?? $abstract;
     }
@@ -39,7 +79,7 @@ class Container implements ContainerInterface
 
     public function has(string $id): bool
     {
-        return isset($this->bindings[$id]) || isset($this->singletonBindings[$id]) || isset($this->instances[$id]);
+        return isset($this->bindings[$id]) || isset($this->singletonBindings[$id]) || isset($this->instances[$id]) || class_exists($id);
     }
 
     public function make(string $abstract, array $parameters = []): mixed
@@ -61,6 +101,9 @@ class Container implements ContainerInterface
             return $this->resolve($this->bindings[$id]);
         }
 
+        if (class_exists($id)) {
+            return $this->resolve($id);
+        }
         throw new NotFoundException("Target [$id] is not bound in the container.");
     }
 }
