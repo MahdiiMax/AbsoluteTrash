f13f434c7b0e8844501f7db5664984103ac284a7
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-23 21:33:08 +0330

    feat: wire auth routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 declare(strict_types=1);
 
 // use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\UserController;
 use Trash\Routing\Facades\Route;
 
 // Route::get('/users/{id}', [HomeController::class, 'show'])->name('users.show');
 Route::get('users', [UserController::class, 'index'])->name('users.index');
 Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login'])->name('login.post');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
