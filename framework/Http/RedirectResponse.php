224ec32ca878da493246ec54de33d46b0cb7b369
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-09-03 22:09:02 +0330

    feat: add RedirectResponse for http redirects

---
+++ framework/Http/RedirectResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Trash\Http;
+
+use Trash\Http\Message\Response;
+use Trash\Routing\Router;
+
+class RedirectResponse extends Response
+{
+    public function __construct(string $url, int $status = 302,)
+    {
+        parent::__construct($status, ["Location" => $url]);
+    }
+
+    public function to(string $url): static
+    {
+        return new static($url, $this->getStatusCode());
+    }
+
+    public function route(string $name): static
+    {
+        $route = app(Router::class)->getByName($name);
+        return new static($route?->getPath() ?? '/', $this->getStatusCode());
+    }
+
+    public function with(string $key, mixed $value): static
+    {
+        session()?->flash($key, $value);
+        return $this;
+    }
+
+    public function withErrors(array $errors): static
+    {
+        return $this->with('errors', $errors);
+    }
+}
