68155fcc04ab42c892ea9ffa9f7476e52e42ef5a
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-09-06 19:29:01 +0330

    feat: add posts routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Trash\Auth\Middleware\Authenticate;
 use Trash\Routing\Facades\Route;
@@ -20,3 +21,8 @@ Route::get('logout', [LoginController::class, 'logout'])->name('logout')->middle
 
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register'])->name('register.post');
+
+Route::get('posts', [PostController::class, 'index'])->name('posts.index')->middleware(Authenticate::class);
+Route::get('posts/create', [PostController::class, 'create'])->name('posts.create')->middleware(Authenticate::class);
+Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show')->middleware(Authenticate::class);
+Route::post('posts', [PostController::class, 'store'])->name('posts.store')->middleware(Authenticate::class);
