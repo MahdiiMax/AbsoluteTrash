d45c3af44c6810c92afed31028aee2fd5136db2f
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-09-09 18:49:13 +0330

    feat: add and register VerifyCsrfToken middleware

--- config/app.php
+++ config/app.php
@@ -22,6 +22,7 @@ return [
         \App\Http\Middleware\AddHeaderMiddleware::class,
         \Trash\Http\Middleware\StartSession::class,
         \Trash\Http\Middleware\ConvertMethod::class,
+        \Trash\Http\Middleware\VerifyCsrfToken::class,
     ],
     'routes' => [
         base_path(fixPathSeparator('routes/web.php')),
