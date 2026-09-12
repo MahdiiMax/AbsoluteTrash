1621ec5288fbe13efbe4b881bef6bfc57a48c4a8
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-09-12 21:03:34 +0330

    feat: add ModelNotFoundException

---
+++ framework/Database/Exceptions/ModelNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Trash\Database\Exceptions;
+
+use RuntimeException;
+
+class ModelNotFoundException extends RuntimeException
+{
+    public function __construct(string $message = 'Model not found')
+    {
+        return parent::__construct($message, 404);
+    }
+}
