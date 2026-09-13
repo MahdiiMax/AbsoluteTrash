10b98b0ddef15d27ca1b30016e63142e06c7a73a
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-09-13 19:18:50 +0330

    feat: add dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Trash\Auth\Middleware\Authenticate;
@@ -14,6 +15,9 @@ use Trash\Routing\Facades\Route;
 // Route::get('/users/{id}', [HomeController::class, 'show'])->name('users.show');
 Route::get('users', [UserController::class, 'index'])->name('users.index')->middleware(Authenticate::class);
 Route::get('users/{id}', [UserController::class, 'show'])->name('users.show')->middleware(Authenticate::class);
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index')->middleware(Authenticate::class);
+Route::post('dashboard/avatar', [DashboardController::class, 'updateAvatar'])->name('dashboard.avatar')->middleware(Authenticate::class);
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login'])->name('login.post');
